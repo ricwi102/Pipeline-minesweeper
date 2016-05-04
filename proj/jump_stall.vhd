6ab16b11058f10ab45e0980094523feb223caa02
Viktor Regard <vikre869@mux1-12.edu.isy.liu.se>
2016-05-04 09:31:33 +0200

    more testing and fixed direct jumps (and <relative onesgit add PM.vhd jump_stall.vhd)

--- proj/jump_stall.vhd
+++ proj/jump_stall.vhd
@@ -26,7 +26,8 @@ architecture Behavioral of jump_stall is
   signal stall_rom     	: std_logic_vector(30 downto 0) := b"0001_0111_1111_1111_1111_1111_1000_000";
   signal IR1_internal   : std_logic_vector(31 downto 0) := (others => '0');
   signal IR2_internal   : std_logic_vector(31 downto 0) := (others => '0');
-  alias command 	: std_logic_vector(5 downto 0) is IR2_internal(31 downto 26);
+  alias command_ir1	: std_logic_vector(5 downto 0) is IR1_internal(31 downto 26);
+  alias command_ir2 	: std_logic_vector(5 downto 0) is IR2_internal(31 downto 26);
 
   signal  mux_signal 	: std_logic_vector(1 downto 0) := "00";
   
@@ -34,6 +35,7 @@ architecture Behavioral of jump_stall is
   alias send_stall 	  : std_logic is mux_signal(1);
   
   signal j, z_flag_j 	: std_logic := '0';
+  signal dir_jump	: std_logic := '0';
   alias take_jump 	: std_logic is mux_signal(0);
 
  
@@ -45,7 +47,7 @@ begin  -- Behoavioral
 
   -- STALL LOGIC --
 
-  mem_acc <= '1' when (command = "000011") else '0';
+  mem_acc <= '1' when (command_ir2 = "000011") else '0';
 
   reg_acc <= '1' when ((IR2_internal(25 downto 21) = IR1_internal(20 downto 16) or
                        (IR2_internal(25 downto 21) = IR1_internal(15 downto 11)))) else '0';
@@ -56,9 +58,12 @@ begin  -- Behoavioral
   
   -- JUMP LOGIC --
 
-  j <= '1' when (command = "011001" or command = "011010") else '0';
+  dir_jump <= '1' when (command_ir1 = "011001" or command_ir1 = "011011" or 
+			command_ir1 = "011101") else '0';
+
+  j <= '1' when (command_ir2 = "011001" or command_ir2 = "011010") else '0';
   
-  z_flag_j <= '1' when (z_flag_in = '1' and (conv_integer(command) >= 27 and conv_integer(command) <= 31))
+  z_flag_j <= '1' when (z_flag_in = '1' and (conv_integer(command_ir2) >= 27 and conv_integer(command_ir2) <= 31))
               else '0';
   
   take_jump <= '1' when (j = '1' or z_flag_j = '1') else '0';
@@ -73,13 +78,18 @@ begin  -- Behoavioral
           PC <= (others => '0');
           PC1 <= (others => '0');
           PC2 <= (others => '0');
-        else    
-          PC2 <= extend + PC1;
+        else
+	  if (dir_jump = '1') then    
+ 	    PC2 <= extend;
+	  else
+	    PC2 <= extend + PC1;
+	  end if;
+
           PC1 <= PC;
-          if(mux_signal = "00") then
+          if (mux_signal = "00") then
             PC <= PC + 1;
-          elsif(mux_signal = "01") then
-            PC <= PC2;
+          elsif (mux_signal = "01") then
+            PC <= PC2;	  
           else
             PC <= PC;
           end if;
@@ -90,8 +100,8 @@ begin  -- Behoavioral
           
 
   extend(24 downto 0) <= IR1_internal(24 downto 0);
-  extend(30 downto 25) <= (others => '0');
-  extend(31) <= IR1_internal(25);
+  extend(31 downto 25) <= (others => IR1_internal(25));  
+
 
   -- MUX --  
   IR1_out_js <= PM_in              when (mux_signal = "00") else
